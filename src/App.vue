3018b138b9a2f59ee19097db531de27b1f697d97
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2021-07-05 17:51:05 -0500

    [ADD]

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Play/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Play from './components/Play.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Play
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
+
+body{
+  min-height: 100vh;
+  min-width: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Play.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="col row between">
+      <div class="square"></div>
+      <div class="square"></div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col row center">
+      <div class="square"></div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col row between">
+      <div class="square"></div>
+       <div class="square"></div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Play',
+  props: {
+    
+  }
+}
+</script>
+
+<style scoped>
+@import 'styles.css';
+</style>
